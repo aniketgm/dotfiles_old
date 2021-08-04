fd70f05b6ed08585ba1174b2c420d8f7797cd586
Aniket Meshram <aniket.g.meshram@gmail.com>
2021-08-04 21:08:24 +0530

    Added fish_prompt.fish

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,20 @@
+function fish_prompt -d Hydro
+
+    # Set symbol before _hydro_pwd
+    if set -q ConEmuIsAdmin
+        set _hydro_preprompt_symbol 'Ψ '
+    else
+        set _hydro_preprompt_symbol 'λ '
+    end
+
+    # Change _hydro_pwd to basename only
+    set _hydro_pwd (basename (prompt_pwd)) 
+    # Set symbol after prompt
+    set _hydro_prompt '››'
+    
+    # Add colors to prompt
+    set -l rand_colors F44336 E91E63 9C27B0 673AB7 3F51B5 2196F3 03A9F4 00BCD4 009688 4CAF50 8BC34A CDDC39 FFEB3B FFC107 FF9800 FF5722 EF9A9A  81D4FA D32F2F C2185B 7B1FA2 512DA8 303F9F 1976D2 0288D1 0097A7 00796B 388E3C 689F38 AFB42B FBC02D FFA000 F57C00 E64A19 5D4037 616161 455A64
+    set -l rand_color_idx (math (random) \% 36 + 1)
+    set_color -o $rand_colors[$rand_color_idx]
+    string unescape "$_hydro_color_pwd$_hydro_preprompt_symbol$_hydro_pwd\x1b[0m $_hydro_color_git$$_hydro_git\x1b[0m$_hydro_color_duration$_hydro_cmd_duration\x1b[0m$_hydro_prompt\x1b[0m "
+end
